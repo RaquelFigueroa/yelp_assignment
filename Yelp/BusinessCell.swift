0bce15ead276a6929960e0068fa278c02c6e0b20
RaquelFigueroa <rfiqueroa@csumb.edu>
2018-02-15 21:27:09 -0800

    Add auto layout of cell

--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -36,8 +36,15 @@ class BusinessCell: UITableViewCell {
         // Initialization code
         thumbView.layer.cornerRadius = 3
         thumbView.clipsToBounds = true
+        
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
     }
 
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
